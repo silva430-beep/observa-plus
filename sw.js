442fe9acdd96bd5a6c45a6b7dcf067c837cdd7a2
SEGURIDAD 24/7 <broddysilva430@gmail.com>
2026-01-14 22:28:58 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,21 +1,18 @@
-const CACHE = "observa-plus-v1";
-
-self.addEventListener("install", (event) => {
+self.addEventListener('install', function(event) {
   event.waitUntil(
-    caches.open(CACHE).then((cache) =>
-      cache.addAll([
-        "./",
-        "./index.html",
-        "./manifest.json",
-        "./icon-192.png",
-        "./icon-512.png"
-      ])
-    )
+    caches.open('observa-plus-v1').then(function(cache) {
+      return cache.addAll([
+        '/',
+        '/index.html'
+      ]);
+    })
   );
 });
 
-self.addEventListener("fetch", (event) => {
+self.addEventListener('fetch', function(event) {
   event.respondWith(
-    caches.match(event.request).then((response) => response || fetch(event.request))
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
   );
 });
